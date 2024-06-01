f068a217817e6791abaeac924b5a9ad3244e3f8b
JohnIanOngayi <ongayijohnian@gmail.com>
2024-06-01 06:57:03 +0300

    Added app that respond to a GET /number/n ly if n is an int
    Response: n is a number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+"""
+Script Starts A Flask Application
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def root():
+    """
+    Returns 'HELLO HBNB' when root is queried
+    """
+    return "HELLO HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    Returns 'HELLO HBNB' when /hbnb is queried
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+    """
+    Returns 'C text' when /c/text is queried
+    """
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_route(text="is cool"):
+    """
+    Returns 'Python text' when /python/text is queried
+    """
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_route(n):
+    """
+    Returns 'n is a number' if n is an int
+    """
+    return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
