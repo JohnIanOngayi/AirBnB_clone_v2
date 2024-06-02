4743a78b87df76e836470b1d7009744a0e1bff6e
JohnIanOngayi <ongayijohnian@gmail.com>
2024-06-02 17:38:37 +0300

    Adding flask app that populates States section

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Script that populates the States tab
+"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def render_state():
+    """Renders the states tab"""
+    states_dicts = storage.all(State).values()
+    return render_template('7-states_list.html', states=states_dicts)
+
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    """Closes current session or deserialises JSON into objects"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
